3326aa027e4bc2d4fd6b357f27504ff7a3e48402
NikToloknov <nikita.toloknov1@gmail.com>
2015-11-15 10:29:18 +0300

    Start of development

---
+++ CathedralProjects/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace CathedralProjects.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ CathedralProjects/Models/Student.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Driver.GeoJsonObjectModel;
+using MongoDB.Shared;
+
+namespace CathedralProjects.Models
+{
+    public class Student
+    {
+        [BsonId]
+        public string ExternalId { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string MiddleName { get; set; }
+        public string GroupId { get; set; }
+
+    }
+}
