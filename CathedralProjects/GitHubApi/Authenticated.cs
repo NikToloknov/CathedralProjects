7c857a23b78b7a7c214bc9fe25edd9cc38036f12
NikToloknov <nikita.toloknov1@gmail.com>
2015-11-27 22:18:31 +0300

    Start develop

---
+++ CathedralProjects/GitHubApi/Authenticated.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Octokit;
+
+namespace CathedralProjects.GitHubApi
+{
+        //    Client ID
+        //1b94452678961f919d63
+        //Client Secret
+        //197f44d4b48295a8d0014b858612b7e571639694
+    public class Authenticated
+    {
+        public const string ClientId = "1b94452678961f919d63";
+        public const string ClientSecret = "197f44d4b48295a8d0014b858612b7e571639694";
+
+    }
+}
---
+++ TestCafedralProjects/TestApiGitgub.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using CathedralProjects;
+
+namespace TestCafedralProjects
+{
+    [TestFixture]
+    public class TestApiGitgub
+    {
+        [SetUp]
+        public void SetUp()
+        {
+
+        }
+
+        [Test]
+        public void Test()
+        {
+
+        }
+
+        [TearDown]
+        public void TerDown()
+        {
+
+        }
+   }
+}
